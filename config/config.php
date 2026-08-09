56ab99da427fbd278f3e180c171f85e812de9408
alexandrenovaes-boop <alexandre.novaes@preve.g12.br>
2026-08-09 12:03:02 -0300

    Alteração do config.php para o projeto da feira do preve

--- config/config.php
+++ config/config.php
@@ -1,3 +1,15 @@
 <?php
 //URL raiz
-define('URL','http://localhost/mvc_php');
+define('URL', 'http://localhost/mvc_php'); //nome da sua pasta
+
+//Pasta raiz
+define('ROOT_PATH', dirname(__DIR__,1));
+
+//Pasta view
+define('VIEW_PATH', ROOT_PATH . '/view');
+
+//Pasta controllers
+define('CONTROLLER_PATH', ROOT_PATH . '/controllers');
+
+//Pasta models
+define('MODEL');
